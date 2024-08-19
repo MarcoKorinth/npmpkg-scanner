2df23db042b45d41abcc0e1ecab236d53cc96c36
Marco Korinth <marco@bizfactory.tech>
2024-08-19 04:57:41 +0200

    feat: add markdown and pdf generator functions

--- outputgenerator.py
+++ outputgenerator.py
@@ -1,5 +1,7 @@
 import json
 
+from markdown_pdf import MarkdownPdf, Section
+
 from appcontext import AppContext
 from sarifparser import SarifParser
 
@@ -19,8 +21,38 @@ class OutputGenerator:
     def generate_outputfile(self) -> None:
         if self.app_context.output_format == "raw":
             self._generate_raw()
-        elif self.app_context.output_format == "json":
-            self._generate_json()
+        elif self.app_context.output_format == "markdown":
+            self._generate_markdown()
+        elif self.app_context.output_format == "pdf":
+            self._generate_pdf()
+
+    def _get_markdown(self) -> str:
+        markdown = "# NPM package analysis\n\n"
+
+        markdown += "## Summary\n\n"
+        markdown += "The following behaviors were detected:\n\n"
+        for behavior in self.sarif_parser.get_behaviors():
+            properties = self.sarif_parser.get_behavior_properties(behavior)
+            if properties is not None:
+                markdown += f"- {properties["name"]} ({properties["id"]})\n"
+
+        markdown += "\n## Behaviors\n\n"
+        for behavior in self.sarif_parser.get_behaviors():
+            properties = self.sarif_parser.get_behavior_properties(behavior)
+            results = self.sarif_parser.get_behavior_results(behavior)
+            if properties is not None and results is not None:
+                markdown += f"### {properties["id"]}\n\n"
+                markdown += f"**{properties["name"]}**\\\n"
+                markdown += f"{properties["description"]}\n\n"
+
+                for result in results:
+                    for loc in result["locations"]:
+                        markdown += f"In `{loc["file"]}` (line: {loc["line"]}):\n"
+                    markdown += "\n```\n"
+                    markdown += f"{result["text"]}\n"
+                    markdown += "```\n"
+
+        return markdown
 
     def _generate_raw(self) -> None:
         with open(self._get_filename("json"), "w") as f:
@@ -29,3 +61,13 @@ class OutputGenerator:
     def _generate_json(self) -> None:
         with open(self._get_filename("json"), "w") as f:
             json.dump({"todo": "This is a placeholder for the json file format"}, f, indent=4)
+
+    def _generate_markdown(self) -> None:
+        with open(self._get_filename("md"), "w") as f:
+            f.write(self._get_markdown())
+
+    def _generate_pdf(self) -> None:
+        markdown = self._get_markdown()
+        pdf = MarkdownPdf()
+        pdf.add_section(Section(markdown))
+        pdf.save(self._get_filename("pdf"))
--- sarifparser.py
+++ sarifparser.py
@@ -1,3 +1,4 @@
+from functools import cache
 import json
 
 
@@ -5,10 +6,40 @@ class SarifParser:
     def __init__(self, sarif_file: str) -> None:
         with open(sarif_file) as json_data:
             self.raw_data = json.load(json_data)
+            self.run = self.raw_data["runs"][0]
 
+    @cache
     def get_behaviors(self):
-        results = self.raw_data["runs"][0]["results"]
+        results = self.run["results"]
         return [x["ruleId"] for x in results]
 
     def check_behavior(self, behavior):
         return behavior in self.get_behaviors()
+
+    @cache
+    def get_behavior_properties(self, behavior: str) -> dict[str, dict] | None:
+        rules = self.run["tool"]["driver"]["rules"]
+        items = map(lambda rule: (rule["id"], rule["properties"]), rules)
+        behavior_properties = {k: v for k, v in list(items)}
+        return behavior_properties.get(behavior)
+
+    @cache
+    def get_results(self) -> dict:
+        items = [(
+            result["ruleId"],
+            {
+                "text": result["message"]["text"],
+                "locations": [{
+                    "file": l["physicalLocation"]["artifactLocation"]["uri"],
+                    "line": (lambda s, e: f"{s}-{e}" if s != e else s)(
+                        l["physicalLocation"]["region"]["startLine"], 
+                        l["physicalLocation"]["region"]["endLine"]
+                    )
+                } for l in result["locations"]]
+            }
+        ) for result in self.run["results"]]
+        return {k: v for k, v in items}
+
+    @cache
+    def get_behavior_results(self, behavior: str) -> list[dict] | None:
+        return [self.get_results()[behavior]] if behavior in self.get_results() else []
